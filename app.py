2070c8516b326376909796ed71a8f7dc57d57c12
Brian Nieto <brian.nieto1@utp.ac.pa>
2024-06-21 19:04:20 -0500

    - Adding CORS support - Improving response

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import joblib
 import numpy as np
 from classes.ModelPipeline import ModelPipeline
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['POST'])
+@cross_origin()
 def predict():
     try:
         data = request.get_json(force=True)
@@ -15,12 +19,16 @@ def predict():
         if model:
             print("Model loaded successfully.")
         prediction = model.predict_proba(data)
-        print(f'prediction: {prediction}')
     except Exception as error:
         print(error)
         return jsonify({'error': error})
     else:
-        return jsonify(prediction.tolist())
+        proba_array = prediction.tolist()[0]
+        print(proba_array)
+        return jsonify({
+                'positive': proba_array[0] * 100,
+                'negative': proba_array[1] * 100
+            })
 
 @app.route('/', methods=['GET'])
 def test():
